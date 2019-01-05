bde345c71b247ece3d352c2dc21b45f1b4ae1074
Nat! <nat@mulle-kybernetik.com>
2019-01-05 15:29:40 +0100

    modernize mulle-sde

---
+++ test/.mulle/share/env/environment-project.sh
@@ -0,0 +1,48 @@
+#
+#
+#
+export PROJECT_NAME="mulle-regex"
+
+
+#
+#
+#
+export PROJECT_IDENTIFIER="mulle_regex"
+
+
+#
+#
+#
+export PROJECT_DOWNCASE_IDENTIFIER="mulle_regex"
+
+
+#
+#
+#
+export PROJECT_UPCASE_IDENTIFIER="MULLE_REGEX"
+
+
+#
+#
+#
+export PROJECT_TYPE="none"
+
+
+#
+#
+#
+export PROJECT_LANGUAGE="c"
+
+
+#
+#
+#
+export PROJECT_DIALECT="c"
+
+
+#
+#
+#
+export PROJECT_EXTENSIONS="c"
+
+
