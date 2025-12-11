68988375e7d38895f39cdfda058600afc0c11a35
Nat! <nat@mulle-kybernetik.com>
2025-12-11 22:05:06 +0100

    maintenance: upgrade mulle-sde tooling to version 3.5.0

---
+++ test/include.h
@@ -0,0 +1,26 @@
+#ifndef mulle__regex__test_include_h__
+#define mulle__regex__test_include_h__
+
+// THIS FILE WILL BE CLOBBERED BY mulle-sde test craft
+
+#include <backtrace.h>
+#include <backtrace-supported.h>
+
+#include <mintomic/mintomic.h>
+#include <mulle-allocator/mulle-allocator.h>
+#include <mulle-atinit/mulle-atinit.h>
+#include <mulle-buffer/mulle-buffer.h>
+#include <mulle-c11/mulle-c11.h>
+#include <mulle-data/mulle-data.h>
+#include <mulle-dlfcn/mulle-dlfcn.h>
+#include <mulle-dtostr/mulle-dtostr.h>
+#include <mulle-fprintf/mulle-fprintf.h>
+#include <mulle-regex/mulle-regex.h>
+#include <mulle-sprintf/mulle-sprintf.h>
+#include <mulle-stacktrace/mulle-stacktrace.h>
+#include <mulle-testallocator/mulle-testallocator.h>
+#include <mulle-thread/mulle-thread.h>
+#include <mulle-utf/mulle-utf.h>
+#include <mulle-vararg/mulle-vararg.h>
+
+#endif /*  */
